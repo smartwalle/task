199f21d7849357eeeafe9ea4a2223b3c4f36f994
Yangfeng <smartwalle@gmail.com>
2018-03-04 11:48:10 +0800

    update

--- demo/main.go
+++ demo/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	var w = sync.WaitGroup{}
+	var w = &sync.WaitGroup{}
 	var t = time.Now()
 	var p = task4go.NewTaskPool(5)
 	p.Run()
@@ -16,43 +16,22 @@ func main() {
 	p.Run()
 	for i:=0;i <1000000; i++ {
 		w.Add(1)
-		p.AddTask(&Job{i: i})
+		p.AddTask(&Job{i: i, w: w})
 	}
-
-	time.Sleep(time.Second * 3)
-	p.Stop()
-
-	p.Run()
-
-	for i:=0;i <100; i++ {
-		p.AddTask(&Job2{i: i})
-	}
-
 	w.Wait()
 	fmt.Println(time.Now().Sub(t))
 }
 
 type Job struct {
 	i int
+	w *sync.WaitGroup
 }
 
 func (this *Job) Do() {
 	for i := 0; i < 1000000; i++ {
 		//fmt.Println("ee")
 	}
-	//this.w.Done()
-	//fmt.Println("do job", this.i)
-	//time.Sleep( time.Second * 3)
-}
-
-type Job2 struct {
-	i int
-}
-
-
-func (this *Job2) Do() {
-	fmt.Println("job2", this.i)
-	//this.w.Done()
+	this.w.Done()
 	//fmt.Println("do job", this.i)
 	//time.Sleep( time.Second * 3)
 }
